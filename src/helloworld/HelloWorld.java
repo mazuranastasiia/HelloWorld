8ee96547d7c1a12d73b9be73b83f2c10c443bd82
w1720335 <w1720335@my.westminster.ac.uk>
2022-03-21 14:42:43 +0000

    initial commit

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -10,12 +10,11 @@ package helloworld;
  * @author w1720335
  */
 public class HelloWorld {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
